20a1811ccf40bfdb0e386e8ea59c1a4eaba92ddf
Anjalika <anjalikudagamage@gmail.com>
2024-08-21 23:08:59 +0530

    create homepage

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,11 +1,31 @@
+import React from "react";
+import WovenImageList from "../../higherOrderComponents/WovenImageList";
+import IconText from "../../higherOrderComponents/ImageText";
+import ImageCarousel from "../../organisms/ImageCarousel";
+import Image1 from "../../../assets/images/ImageCarousel/image1.jpg";
+import Image2 from "../../../assets/images/ImageCarousel/image2.jpg";
 
+const HomePage: React.FC = () => {
+  const items = [
+    {
+      image: Image1,
+      title: "Title 1",
+      text: "This is the first image",
+    },
+    {
+      image: Image2,
+      title: "Title 2",
+      text: "This is the second image",
+    },
+  ];
 
-const HomePage = () => {
   return (
-    <div>
-      <p>dfghjk</p>
-    </div>
-  )
-}
+    <>
+      <WovenImageList />
+      <IconText />
+      <ImageCarousel items={items} topic="Why choose us ?" />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
