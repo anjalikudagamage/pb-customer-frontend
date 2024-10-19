728e57a0152c02b570dc8598cb2a8e5878ced7eb
Anjalika <anjalikudagamage@gmail.com>
2024-10-19 22:39:25 +0530

    Add styles

--- src/components/organisms/PhotographerImage/index.tsx
+++ src/components/organisms/PhotographerImage/index.tsx
@@ -14,7 +14,21 @@ const PhotographerImage: React.FC<PhotographerImageProps> = ({
   description,
 }) => {
   return (
-    <Box sx={{ ...heroContainer, backgroundImage: `url(${imageUrl})` }}>
+    <Box sx={{ ...heroContainer, overflow: "hidden" }}>
+      <Box
+        sx={{
+          backgroundImage: `url(${imageUrl})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          zIndex: -1,
+        }}
+      />
       <Typography variant="h4" sx={title}>
         {photographerName}
       </Typography>
