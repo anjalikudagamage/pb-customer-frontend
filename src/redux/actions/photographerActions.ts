d9f1d3adc05354caf1e38624654a19cbf24c49fd
Anjalika <anjalikudagamage@gmail.com>
2024-11-17 20:13:54 +0530

    Fix the photographer integration

--- src/redux/actions/photographerActions.ts
+++ src/redux/actions/photographerActions.ts
@@ -1,20 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { apiClient } from "../../api/axiosClient";
-import { AxiosError } from "axios";
+import { fetchPhotographersService } from "../../services/photographerService";
 
 export const fetchPhotographers = createAsyncThunk(
   "photographer/fetchAll",
   async (_, { rejectWithValue }) => {
     try {
-      const response = await apiClient.get("/photographer/allData");
-      return response.data;
+      return await fetchPhotographersService();
     } catch (error) {
-      if (error instanceof AxiosError) {
-        return rejectWithValue(
-          error.response?.data?.message || "Failed to fetch photographers"
-        );
-      }
-      return rejectWithValue("An unknown error occurred");
+      return rejectWithValue(error);
     }
   }
 );
