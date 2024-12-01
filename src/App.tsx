28610c671953302b18477ea8a55fdf596c567ce1
Anjalika <anjalikudagamage@gmail.com>
2024-12-01 11:40:51 +0530

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
-import { Provider } from "react-redux"; // Import Provider from react-redux
-import store from "./redux/store"; // Import your Redux store
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import AppRouter from "./router";
 
 const cache = createCache({ key: "css", prepend: true });
 
 function App() {
   return (
-    <Provider store={store}> {/* Wrap AppRouter with Provider */}
+    <Provider store={store}>
       <CacheProvider value={cache}>
         <div>
           <AppRouter />
